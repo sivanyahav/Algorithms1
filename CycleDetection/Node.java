653a61d357c64b9b22b022c9b0a6555784b81c0f
Sivan Yahav <68699211+sivanyahav@users.noreply.github.com>
2021-03-02 12:27:14 +0200

    Add files via upload

---
+++ CycleDetection/Node.java
@@ -0,0 +1,19 @@
+package CycleDetection;
+
+public class Node {
+
+	int id;
+	static int id_counter = 0;
+	Node next;
+
+	public Node() {
+		this.next = null;
+		this.id = id_counter++;
+	}
+
+	@Override
+	public String toString() {
+		return "("+id+")->" ;
+	}
+
+}
