311bfdb2487ec64ed0fe2f944e9a8473706b8c3c
Sivan Yahav <68699211+sivanyahav@users.noreply.github.com>
2021-03-07 21:36:54 +0200

    Add files via upload

---
+++ Recursion_VS_Induction/factorial.java
@@ -0,0 +1,29 @@
+package Recursion_VS_Induction;
+
+public class factorial {
+	
+	 //O(2N)
+    public static int factorialRecursive(int n) {
+        if( n == 0 )
+            return 1; 
+
+        return n * factorialRecursive(n-1);
+    }
+
+    //O(N)
+    public static int factorialInductive(int n) {
+
+        int ans = 1;
+        for(int i = 1 ; i <= n ; i++) {
+            ans *= i;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorialRecursive(5));
+        System.out.println(factorialInductive(5));
+    }
+
+}
---
+++ Recursion_VS_Induction/package-info.java
@@ -0,0 +1,1 @@
+package Recursion_VS_Induction;
