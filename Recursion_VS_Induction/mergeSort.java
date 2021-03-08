934ebb886678347563a95dfe9b5455a6d8aff3f7
Sivan Yahav <68699211+sivanyahav@users.noreply.github.com>
2021-03-08 15:38:59 +0200

    Update mergeSort.java

--- Recursion_VS_Induction/mergeSort.java
+++ Recursion_VS_Induction/mergeSort.java
@@ -3,16 +3,17 @@ package Recursion_VS_Induction;
 public class mergeSort {
 
 	public void mergeSort(int[] arr, int low, int high) {
+		
 		if(low<high-1) {
 			// Find the middle point
-            int mid = (low+high)/2;
+            		int mid = (low+high)/2;
  
-            // Sort first and second halves
-            mergeSort(arr, low, mid);
-            mergeSort(arr, mid + 1, high);
+            		// Sort first and second halves
+            		mergeSort(arr, low, mid);
+            		mergeSort(arr, mid + 1, high);
  
-            // Merge the sorted halves
-            merge(arr[low,mid],arr[mid,high]);
+            		// Merge the sorted halves
+            		merge(arr[low,mid],arr[mid,high]);
 		}
 	}
 }
